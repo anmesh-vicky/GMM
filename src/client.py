915c35c49f8f8c78af3971c52beb0b795fd25ae9
anmesh-vicky <anmesh231@gmail.com>
2018-05-18 23:34:09 +0530

    basic action_server implemented

---
+++ src/client.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+from __future__ import print_function
+import rospy
+
+# Brings in the SimpleActionClient
+import actionlib
+
+# Brings in the messages used by the fibonacci action, including the
+# goal message and the result message.
+import GMM.msg
+
+def Mission_client():
+    # Creates the SimpleActionClient, passing the type of the action
+    # (FibonacciAction) to the constructor.
+    client = actionlib.SimpleActionClient('Mission_planner', GMM.msg.GMMAction)
+
+    # Waits until the action server has started up and started
+    # listening for goals.
+    client.wait_for_server()
+
+    # Creates a goal to send to the action server.
+    goal = GMM.msg.GMMGoal(order=0)
+
+    # Sends the goal to the action server.
+    client.send_goal(goal)
+
+    # Waits for the server to finish performing the action.
+    client.wait_for_result()
+
+    # Prints out the result of executing the action
+    return client.get_result()  # A FibonacciResult
+
+if __name__ == '__main__':
+    rospy.init_node('mission_planner_py')
+    result = Mission_client()
+   
